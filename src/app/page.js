4a4b09959f5227a15f829867c4d7b94f0d411b53
Lautaro Aguilar <97558288+lautaroAguilar@users.noreply.github.com>
2024-07-05 11:26:47 -0300

    user image and background color

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,27 @@
 import Image from "next/image";
+import userImage from "../../public/userImage.jpg";
 import styles from "./page.module.css";
 import Navbar from "@/components/Navbar/Navbar";
 import Section from "@/components/Section/Section";
+import Circles from "@/components/Circles/Circles";
 export default function Home() {
   return (
     <main className={styles.main}>
       <header className={styles.header}>
         <Navbar />
-        <div className={styles.brand}>Lautaro Aguilar</div>
+        <div className={styles.user_info}>
+          <h4>Lautaro Aguilar</h4>
+          <Image
+            src={userImage}
+            alt="Imagen de usuario"
+            style={{ height: "auto", width: "30px", borderRadius: "50%"}}
+          />
+        </div>
       </header>
-      <section>
+      <section className={styles.section}>
         <Section />
       </section>
+      <Circles />
     </main>
   );
 }
